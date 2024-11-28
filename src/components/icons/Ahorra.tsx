a18f9e8a0c1d67dbb2841106fddfaaa7223d3864
Edwin Baquaix <baquiaxfernando0@gmail.com>
2024-11-27 19:19:21 -0600

    correccion de importacines

--- src/components/icons/Ahorra.tsx
+++ src/components/icons/Ahorra.tsx
@@ -1,4 +1,4 @@
-import AhorraIcon from "../../assets/Ahorra.svg";
+
 
 const Ahorra = () => {
   return <img src="https://micoope.com.gt/wp-content/uploads/2024/03/Ahorra_inicio.svg" style={{width:"120px"}}></img>;
